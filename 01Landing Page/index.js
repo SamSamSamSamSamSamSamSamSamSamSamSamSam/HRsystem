5fe59234f3b265b6866eb06556522330357d69b9
blaxlistZ <blaxlistz@gmail.com>
2023-03-14 12:05:29 +0800

    "Added navigation hover function ."

--- 01Landing Page/index.js
+++ 01Landing Page/index.js
@@ -12,3 +12,24 @@ toggle_button.addEventListener("click", () => {
     toggle_button.innerHTML = `<i class="bx bx-x" style="font-size: 80px;"></i>`;
   }
 });
+
+// Navigation Links
+const tab_buttons = document.querySelectorAll(".tab-button");
+const navigation = document.querySelector(".nav-links");
+const contents = document.querySelectorAll(".links-content");
+
+navigation.addEventListener("mouseover", (e) => {
+  const id = e.target.dataset.id;
+  console.log(id);
+  const element = document.getElementById(id);
+  if (id) {
+    tab_buttons.forEach((button) => {
+      button.classList.remove("active");
+      e.target.classList.add("active");
+    });
+    contents.forEach((content) => {
+      content.classList.remove("active");
+    });
+    element.classList.add("active");
+  }
+});
