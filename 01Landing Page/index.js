2f9d204494e0b2f9cc74d677b6957511e63ff8b5
blaxlistZ <blaxlistz@gmail.com>
2023-03-14 18:55:33 +0800

    "Updated HTM,L, CSS, JS."

--- 01Landing Page/index.js
+++ 01Landing Page/index.js
@@ -1,21 +1,19 @@
-// Sidebar
-const toggle_button = document.querySelector(".menu");
-const close_button = document.querySelector(".close-button");
+const toggleButton = document.querySelector(".menu");
+const closeButton = document.querySelector(".close-button");
 const sidebar = document.querySelector(".sidebar");
 
-toggle_button.addEventListener("click", () => {
+toggleButton.addEventListener("click", () => {
   if (sidebar.classList.contains("show-sidebar")) {
     sidebar.classList.remove("show-sidebar");
-    toggle_button.innerHTML = `<i class='bx bx-menu'></i>`;
+    toggleButton.innerHTML = `<i class='bx bx-menu'></i>`;
   } else {
     sidebar.classList.add("show-sidebar");
-    toggle_button.innerHTML = `<i class="bx bx-x" style="font-size: 80px;"></i>`;
+    toggleButton.innerHTML = `<i class="bx bx-x" style="font-size: 80px;"></i>`;
   }
 });
 
 // Navigation Links
 const buttons = document.querySelectorAll(".tab-button");
-const contents = document.querySelectorAll(".links-content");
 
 buttons.forEach((button) => {
   button.addEventListener("mouseover", () => {
@@ -30,8 +28,8 @@ buttons.forEach((button) => {
   });
 });
 
-function hideAllContents() {
+hideAllContents = () => {
   contents.forEach((content) => {
     content.classList.remove("active");
   });
-}
+};
